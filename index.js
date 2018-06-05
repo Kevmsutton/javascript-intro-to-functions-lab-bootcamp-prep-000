6a67ebe01471fff3a9b52cb8318bd98494c5334b
kevmsutton@hotmail.co.uk <kevmsutton@hotmail.co.uk>
2018-06-05 15:50:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,12 +9,3 @@ function whisper(string){
 function logShout(string){
   console.log (string.toUpperCase())
 }
-
-function logWhisper(string){
-  console.log (string.toLowerCase())
-}
-
-function sayHiToGrandma(string){
-  return string.toLowerCase("I can't hear you")
-}
-
