5a2713099be2c7552846b37d0b01a29c93e9df17
kevmsutton@hotmail.co.uk <kevmsutton@hotmail.co.uk>
2018-06-05 15:30:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,5 +14,5 @@ function logWhisper(string){
   console.log (string.toLowerCase())
 }
 function sayHiToGrandma(string){
- return string.toLowerCase(I cant hear you!)
+ return string.toLowerCase("I cant hear you!")
 }
